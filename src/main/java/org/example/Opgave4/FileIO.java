0ceb2d6461bcbd7fe75a0b75c5028a6e9a964c8d
magnewei <magnewei@icloud.com>
2023-12-30 07:54:31 +0100

    Refactor Quiz

---
+++ src/main/java/org/example/Opgave4/FileIO.java
@@ -0,0 +1,8 @@
+package org.example.Opgave4;
+
+public class FileIO {
+    public void saveToFile() {
+
+    }
+
+}
--- src/main/java/org/example/Opgave4/Loops.java
+++ src/main/java/org/example/Opgave4/Loops.java
@@ -6,17 +6,24 @@ public class Loops {
 
     // Print left side of a triangle.
     public void drawLeftTriangle(int i) {
-        for(int n = 1; n < i ; n++) {
+        for (int n = 1; n < i; n++) {
             // String.repeat will, as given by the method call, repeat a string n times.
-            System.out.println(whiteSpace.repeat(10-n) + star.repeat(n));
+            System.out.println(whiteSpace.repeat(i - n) + star.repeat(n));
         }
     }
 
     // Print right side of a triangle.
     public void drawRightTriangle(int i) {
-        for(int n = 1;  n < i ; n++) {
+        for (int n = 1; n < i; n++) {
             // String.repeat will, as given by the method call, repeat a string n times.
-            System.out.println(star.repeat(n) + whiteSpace.repeat(10-n));
+            System.out.println(star.repeat(n) + whiteSpace.repeat(i - n));
+        }
+    }
+
+    public void drawSquare(int i) {
+        for (int n = 1; n < i; n++) {
+            // String.repeat will, as given by the method call, repeat a string n times.
+            System.out.println(star.repeat(i * 2));
         }
     }
 }
